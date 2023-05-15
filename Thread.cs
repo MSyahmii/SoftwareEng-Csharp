902d645631f69220ccd3d7ec07f39ca341f60a76
MSyahmii <84788168+MSyahmii@users.noreply.github.com>
2023-05-15 11:16:50 +0800

    Add files via upload

---
+++ Thread.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace asynchronous
+{
+    class Program
+    {
+
+        static bool done;
+        //Thread safety
+        static readonly object tupperware = new object();
+        static void Main(string[] args)
+        {
+            //Thread t1 = new Thread(WriteY);
+            //t1.Start();
+            //WriteX();
+
+            new Thread(Go).Start();
+            new Thread(Go).Start();
+
+
+            Console.Read();
+        }
+
+        static void Go()
+        {
+            lock (tupperware)
+            {
+                if (!done)
+                {
+                    Console.WriteLine("Done");
+                    done = true;
+                }
+            }
+        }
+
+        static void WriteX()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("x");
+            }
+        }
+        static void WriteY()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("y");
+            }
+        }
+    }
+
+    
+}
