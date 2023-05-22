a8133f9cd7a6d66898b1f2c843ebb918eab1ec95
MSyahmii <84788168+MSyahmii@users.noreply.github.com>
2023-05-22 11:10:40 +0800

    Add files via upload

---
+++ Parallel_SE/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parallel_SE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long number = long.Parse(textBox1.Text);
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            bool is_prime = Prime(number);
+
+            sw.Stop();
+
+            if (is_prime)
+            {
+                MessageBox.Show("It is prime number. \n\nTime taken (ms) = " + sw.ElapsedMilliseconds.ToString());
+            }
+            else
+            {
+                MessageBox.Show("It is not prime number. \n\nTime taken (ms) = " + sw.ElapsedMilliseconds.ToString());
+            }
+        }
+
+            private bool Prime(long number)
+            {
+            bool result=true;
+                Parallel.For( 2, number/2 + 1, i =>
+                {
+                    if (number % i == 0)
+                    {
+                        result = false;
+                    }
+            });
+            return result;
+        }
+    }
+}
